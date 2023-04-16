dfab7e0fd6ec4ab851646e7f8baf9f17e96071b0
NextConfigScripts <130928023+NextConfigScripts@users.noreply.github.com>
2023-04-16 20:48:53 +0200

    Update wings_update.sh

--- wings_update.sh
+++ wings_update.sh
@@ -4,9 +4,9 @@ echo "Creando screen..."
 screen -dmS updater_wings
 echo "Ejecutando comandos..."
 screen -S updater_wings -X stuff 'systemctl stop wings && curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")" && chmod u+x /usr/local/bin/wings && systemctl restart wings\n'
-echo "Esperando 7 segundos y matando la screen..."
-sleep 7s
-screen -S updater_wings -X stuff 'quit\n'
+echo "Esperando 5 segundos y matando la screen..."
+sleep 5s
+screen -S updater_wings -X stuff 'exit\n'
 echo "Nos vemos en 7 días!"
 sleep 15d
 done
